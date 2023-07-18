4a8af7cb77c86a1282625e69fa0d3ab9a47dd430
“guozizheng” <2575424851@qq.com>
2023-07-18 21:12:53 +0800

    ss

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Gs Blog`;
-export const SITE_DESCRIPTION = 'Blog';
+export const SITE_TITLE = `GuoZZ's Blog`;
+export const SITE_DESCRIPTION = '这是郭子正的Blog';
 export const SITE_EMAIL = '2575424851@qq.com'
 export const SITE_NAME = 'www.guozizheng.cloud';
 export const SITE_URL = "www.guozizheng.cloud";
@@ -8,4 +8,4 @@ export const SITE_LANG = "zh-CN";
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "guozizheng";
+export const SITE_AUTHOR = "郭子正";
