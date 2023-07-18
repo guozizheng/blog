9cf6b0d822ed1905d5666916fd7ce862c1923545
“guozizheng” <2575424851@qq.com>
2023-07-18 21:16:51 +0800

    s

--- src/consts.js
+++ src/consts.js
@@ -8,4 +8,4 @@ export const SITE_LANG = "zh-CN";
 // Author, used only when author is not specified on the page, 
 // has lower priority than the author specified in the md file.
 // 作者信息，仅用在文章没有指定作者的时候，优先显示在 md 文件中指定的作者。
-export const SITE_AUTHOR = "郭子正";
+export const SITE_AUTHOR = "郭子正Blog";
